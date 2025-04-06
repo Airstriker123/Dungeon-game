411e4dc93b61d17b4255c41dd6423211559ed479
Airstriker <114228623+Airstriker123@users.noreply.github.com>
2025-04-06 20:00:41 +1000

    Fixed animation not render
    Fixed animation

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ import random
 # Day 1 :0 21
 #completed character!
 class Game:
-    def __init__(self
-                 ):
+    def __init__(self):
         pygame.init()  # Start pygame
-        self.screen = pygame.display.set_mode((800, 600),pygame.FULLSCREEN)
+        self.screen = pygame.display.set_mode((800, 600), pygame.FULLSCREEN)
         self.clock = pygame.time.Clock()
         self.running = True
 
@@ -21,38 +20,40 @@ class Game:
         # Create your character once
         self.main_character = Character("Hero", 100, 10, 10, 8, 12, 50)
 
-    def run(self)\
-            :
-            while self.running:
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        self.running = False
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
 
-                # Get key presses and move the character
-                keys = pygame.key.get_pressed()
-                self.main_character.move(keys)
+            # Get key presses and move the character
+            keys = pygame.key.get_pressed()
+            self.main_character.move(keys)
+            self.main_character.update_animation()
 
-                # Update animation
-                self.main_character.update_animation()
+            # Fill screen
+            self.screen.fill((100, 100, 100))
+    
+            # Draw character
+            self.main_character.draw(self.screen)
 
-                # bg color
-                self.screen.fill((10, 255, 130))
-
-                # Draw character
-                self.main_character.draw(self.screen)
-
-                # Update display
-                pygame.display.flip()
-                self.clock.tick(60)  # Max FPS
+            # Update display
+            pygame.display.flip()
+            self.clock.tick(60)  # Max FPS
+            # main game loop
 
 
 
 
-# run game
-game = Game(
+        pygame.quit(
+
+        )
+
+(
+       Game(
 
 )
-game.run(
+     .run(
 
+    )
 )
-
